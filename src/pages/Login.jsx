4ef65ef3b505b0e0ea8c03439a805d939341e641
Frengk Iskandar <frankierockabilly@gmail.com>
2025-10-19 19:46:14 +0700

    okey

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import profile from '../assets/profile.png'
 import { useNavigate } from 'react-router-dom';
-
+import bgLogin from "../assets/log.png";
 // icons from react icons
-import { FaUserAlt, FaKey,FaEye,FaEyeSlash } from "react-icons/fa";
-import axios from 'axios';
-import { Alert, Button } from '@mui/material';
+import { FaUserAlt, FaKey, FaEye, FaEyeSlash } from "react-icons/fa";
+import axios from "axios";
+import { Alert, Button } from "@mui/material";
 import SuccessAlert from "../alerts/SuccessAlert";
 import ErrorAlert from "../alerts/ErrorAlert";
 
@@ -50,11 +50,11 @@ const Login = () => {
          }, 3000);
       } catch (error) {
          setErrorLogin(true);
-        setMessage(error.response.data.msg);
-        setLoading(false);
-        setTimeout(() => {
-           setErrorLogin(false);
-        }, 2000);
+         setMessage(error.response.data.msg);
+         setLoading(false);
+         setTimeout(() => {
+            setErrorLogin(false);
+         }, 2000);
       } finally {
          setLoading(false);
       }
@@ -62,7 +62,7 @@ const Login = () => {
 
    return (
       <>
-         <div className="w-full h-screen flex justify-center items-center shadow-lg px-20 py-20 bg-purple-100 relative">
+         <div className="w-full h-screen flex justify-center items-center shadow-lg p-28 relative bg-fuchsia-100">
             {/* jika sukses login */}
             {succesLogin && <SuccessAlert message={message} />}
             {errorLogin && <ErrorAlert message={message} />}
@@ -78,7 +78,7 @@ const Login = () => {
             )}
             <div className="flex justify-between items-center w-full h-full bg-white rounded-3xl shadow-2xl">
                {/*kiri  */}
-               <div className="w-[60%] h-full flex  justify-center items-center ">
+               <div className="w-[50%] h-full flex  justify-center items-center ">
                   <div className="flex flex-col justify-start items-start gap-5">
                      <h1 className="font-bold text-5xl">Login</h1>
 
@@ -86,7 +86,7 @@ const Login = () => {
                         className="my-5 flex flex-col justify-start items-start gap-5"
                         onSubmit={handleLogin}
                      >
-                        <div className="px-4 py-3 w-[30rem] bg-gray-200 flex justify-start items-center gap-5 rounded-full border border-violet-900 ">
+                        <div className="px-6 py-3 w-[30rem] bg-gray-200 flex justify-start items-center gap-5 rounded-full ">
                            <FaUserAlt size={20} />
                            <input
                               type="text"
@@ -98,7 +98,7 @@ const Login = () => {
                            />
                         </div>
 
-                        <div className="px-4 py-3 w-[30rem] bg-gray-200 flex justify-between items-center gap-5 rounded-full border border-violet-900 ">
+                        <div className="px-6 py-3 w-[30rem] bg-gray-200 flex justify-between items-center gap-5 rounded-full ">
                            <FaKey size={20} />
                            <input
                               type={hidePassword ? "text" : "password"}
@@ -139,7 +139,7 @@ const Login = () => {
                </div>
                {/* kanan */}
                <div
-                  className="w-full h-full p-10 flex flex-1 flex-col justify-start items-end gap-3 text-white bg-vioborder-violet-900 border border-red-300 rounded-e-3xl font-poppins"
+                  className="w-full h-full p-10 flex flex-1 flex-col justify-start items-end gap-3 text-white bg-vioborder-violet-900 rounded-e-3xl font-poppins"
                   style={{
                      backgroundImage: `url(${profile})`,
                      backgroundPosition: "left",
